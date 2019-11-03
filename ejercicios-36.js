a4419d33ecb2e613d190fc0a93e1ac310cdcb9ae
unknown <lbelliski@gmail.com>
2019-11-02 22:45:17 -0300

    primer commit

---
+++ ejercicios-36.js
@@ -0,0 +1,36 @@
+
+//ejercicio2//
+// let numero = prompt ("Por favor ingresá un número")
+// if (numero % 2 ===0) { alert (`El número ingresado es par`)}
+// else { alert ('El número es impar')};
+
+// //ejercicio 3 //
+// let edad = prompt ("Por favor ingresá tu edad")
+// if (edad >= 18 ) { alert ( `Usted es mayor de edad`)}
+// else { alert ( `Usted es menor de edad`)};
+
+// //ejercicio 1 con else//
+// if (usuarioIngresado === usuario) {
+//     let passwordIngresado = prompt(`Cual es tu password?`)
+//     if (passwordIngresado === password) {
+//       alert(`Bienvenide, ${usuario}`)
+//     };
+//   }
+//   else {
+//     alert(`Ingresaste mal tus datos, por favor intentalo nuevamente`)
+//   };
+  
+  //ejercicio con operador ternario//
+
+// let numero = prompt ("Por favor ingresá un número")
+// numero % 2 ===0 ? alert (`El número ingresado es par`):alert ('El número es impar');
+// let edad = prompt ("Por favor ingresá tu edad")
+// edad >= 18 ? alert ( `Usted es mayor de edad`) : alert ( `Usted es menor de edad`);
+
+//EJERCICIO 6
+let horaActual = prompt(`Hola, qué hora es?`)
+
+if (horaActual >=5 && horaActual <=12) {alert ("Buen día")}
+else if (horaActual >=13 && horaActual <=20) {alert ("Buenas tardes")}
+else {alert ("Buenas noches")};
+
